cc00ff0046e43d74a5445e774e35399f73ccf541
akarreg <161454874+akarreg@users.noreply.github.com>
2024-11-05 11:27:09 +0100

    make a frist react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h2> Hello to react</h2>
+          <ClimbingBoxLoader color="#5fd02a" size={23} />
         </p>
         <a
           className="App-link"
@@ -16,6 +20,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <Skeleton count={3} />
         </a>
       </header>
     </div>
